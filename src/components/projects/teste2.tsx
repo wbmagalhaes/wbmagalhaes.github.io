ebcf708a49be84ae98b0218d31e991c18095ffe6
William <william1504@gmail.com>
2022-11-10 18:43:52 -0300

    mdx pages

---
+++ src/components/projects/teste2.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+interface Props {}
+
+export default function Teste2({}: Props) {
+	const [clicks, setClicks] = useState(0);
+
+	function onClick() {
+		setClicks(clicks + 1);
+	}
+
+	return <button onClick={onClick}>{clicks}</button>;
+}
