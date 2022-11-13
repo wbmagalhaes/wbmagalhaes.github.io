b59c773c1dacb2be5c4bb7bb0a2895d8ea388a7d
William <william1504@gmail.com>
2022-11-13 19:50:36 -0300

    true?

--- src/projects/mandelbrot/index.tsx
+++ src/projects/mandelbrot/index.tsx
@@ -1,6 +1,6 @@
 import loadable from '@loadable/component';
 
-const MandelbrotRender = loadable(() => import('./MandelbrotSketch'), { ssr: false });
+const MandelbrotRender = loadable(() => import('./MandelbrotSketch'), { ssr: true });
 
 export function MandelbrotSet() {
 	return <MandelbrotRender scale={1.8} offset={[-0.5, 0]} />;
