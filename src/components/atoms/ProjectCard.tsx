adacc936dd1812745c17be367d38ffe76e609b27
William <william1504@gmail.com>
2022-11-17 10:25:02 -0300

    image min size

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -9,9 +9,13 @@ interface Props {
 
 export default function ProjectCard({ url, cover, title, description, tags, date }: Props) {
 	return (
-		<article className="max-w-xs flex flex-col bg-wm-secondary rounded-lg shadow-xl text-left overflow-hidden">
-			<a href={url}>
-				<img src={cover} className="w-full m-auto text-center" alt="Imagem de Capa" width="auto" height={256} />
+		<article className="w-full max-w-xs flex flex-col bg-wm-secondary rounded-lg shadow-xl text-left overflow-hidden">
+			<a href={url} className="w-full h-48 bg-cover m-auto text-center overflow-hidden">
+				<img
+					src={cover}
+					className="min-w-full min-h-full flex justify-center items-center"
+					alt="Imagem de Capa"
+				/>
 			</a>
 
 			<div className="flex-grow p-5">
