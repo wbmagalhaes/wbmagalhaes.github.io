f215aee44355e1ec9f9410e796fcf334528fc8d0
William <william1504@gmail.com>
2022-11-19 15:44:00 -0300

    projects data

--- src/components/core/Path.ts
+++ src/components/core/Path.ts
@@ -1,3 +1,7 @@
-export function getSlugFromPath(path?: string) {
-	return path?.split('/').reverse()[0].split('.')[0];
+export function getSlugFromURL(url?: string) {
+	return url?.split('/').reverse()[0].split('.')[0];
+}
+
+export function getLocaleFromURL(url?: string) {
+	return url?.split('/')[2];
 }
