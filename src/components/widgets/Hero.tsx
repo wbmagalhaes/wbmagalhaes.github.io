7df7883542bda797fbd5c64c46877ec326d1c2ec
William <william1504@gmail.com>
2022-12-15 12:53:30 -0300

    hero update

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -3,10 +3,10 @@ import { DownArrow } from '../atoms/DownArrow';
 export default function Hero() {
 	return (
 		<section className="h-full min-h-screen flex flex-col px-4 pb-8 sm:px-8 pt-32">
-			<div className="flex flex-col">
+			<div className="flex flex-col lg:m-8">
 				<h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-none">William Magalhães</h1>
-				<h2 className="text-1xl sm:text-2xl md:text-3xl font-semibold py-2">Full-stack Developer</h2>
-				<p className="max-w-lg text-justify py-2">
+				<h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold py-2">Full-stack Developer</h2>
+				<p className="max-w-xl text-justify py-2">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
 					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
 					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
