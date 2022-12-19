47188dfeb3f7c01ba6b1190ab5ce86188c25a684
William <william1504@gmail.com>
2022-12-19 19:50:33 -0300

    tech stack

---
+++ src/components/atoms/TechStack.tsx
@@ -0,0 +1,39 @@
+import { Icon } from '@iconify/react';
+
+type TechProps = {
+	name: string;
+	icon: string;
+	color: string;
+};
+
+export type TechListProps = {
+	title: string;
+	list: TechProps[];
+};
+
+type TechStackProps = {
+	techs: TechListProps[];
+};
+
+export default function TechStack({ techs }: TechStackProps) {
+	return (
+		<>
+			{techs.map(({ title, list }, i) => (
+				<div className="py-2" key={i}>
+					<h2 className="py-2">{title}</h2>
+					<ul className="flex flex-wrap gap-4">
+						{list.map(({ name, icon, color }, i) => (
+							<li
+								key={i}
+								className="text-sm inline-flex rounded-full px-3 py-1 gap-3 items-center bg-wm-carbon-600"
+							>
+								<span>{name}</span>
+								<Icon icon={icon} color={color} />
+							</li>
+						))}
+					</ul>
+				</div>
+			))}
+		</>
+	);
+}
