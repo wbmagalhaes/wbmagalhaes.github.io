b976bfc46fe0342a73693bc20d52ed61fb0c0f76
William <william1504@gmail.com>
2022-12-19 12:38:54 -0300

    empty link

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -7,10 +7,10 @@ export default function Hero() {
 				<h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-none">William Magalhães</h1>
 				<h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold py-2">Full-stack Developer</h2>
 				<p className="max-w-xl lg:max-w-2xl text-justify py-2">
+					Short, one paragraph text about me.
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
 					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur.
+					aliquip ex ea commodo consequat.
 				</p>
 			</div>
 
