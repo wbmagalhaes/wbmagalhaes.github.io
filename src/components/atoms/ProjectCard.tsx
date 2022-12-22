d9f7d4c8bbb3fd7efb855caea706713c38cdf6b2
William <william1504@gmail.com>
2022-12-22 12:16:53 -0300

    featured

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { OpenProjectButton } from '@atoms/OpenProjectButton';
 import type { ProjectProps } from '@atoms/ProjectShowcase';
 
 interface Props {
@@ -9,7 +10,7 @@ export default function ProjectCard({ project, noCover = false }: Props) {
 	const aria = `Abrir ${project.title}`;
 
 	return (
-		<article className="w-full max-w-sm flex flex-col bg-wm-carbon-300 rounded text-left overflow-hidden hover:-translate-y-1 transition-all duration-200">
+		<article className="w-full max-w-sm flex flex-col bg-wm-carbon-300 rounded text-left overflow-hidden hover:-translate-y-1 transition-all duration-200 hover:shadow hover:shadow-wm-oxygen-900 ">
 			{!noCover && (
 				<a href={project.url} aria-label={aria} className="w-full h-48 grid place-items-center overflow-hidden">
 					<img
@@ -57,28 +58,5 @@ export default function ProjectCard({ project, noCover = false }: Props) {
 				{!noCover && <OpenProjectButton url={project.url ?? '#'} aria={aria} />}
 			</div>
 		</article>
-	);
-}
-
-function OpenProjectButton({ url, aria }: { url: string; aria: string }) {
-	return (
-		<a
-			href={url}
-			aria-label={aria}
-			className="flex gap-2
-					ml-auto items-center
-					text-sm font-medium text-wm-oxygen hover:text-wm-oxygen-200"
-		>
-			<svg
-				className="w-6 h-6"
-				aria-hidden="true"
-				fill="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path d="M14 4c0 .55.45 1 1 1h2.59l-9.13 9.13a.996.996 0 1 0 1.41 1.41L19 6.41V9c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1h-5c-.55 0-1 .45-1 1z" />
-				<path d="M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h5c.55 0 1-.45 1-1s-.45-1-1-1H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-.55-.45-1-1-1s-1 .45-1 1v5c0 .55-.45 1-1 1z" />
-			</svg>
-		</a>
 	);
 }
--- src/components/atoms/ProjectFeatured.tsx
+++ src/components/atoms/ProjectFeatured.tsx
@@ -1,3 +1,4 @@
+import { OpenProjectButton } from '@atoms/OpenProjectButton';
 import type { ProjectProps } from '@atoms/ProjectShowcase';
 
 type Props = {
@@ -13,21 +14,26 @@ export default function ProjectFeatured({ idx, project }: Props) {
 			<a
 				href={project.url}
 				aria-label={aria}
-				className="h-64 aspect-[16/9] grid place-items-center overflow-hidden grow bg-wm-oxygen rounded-md overflow-hidden"
+				className="max-w-lg h-auto grid place-items-center overflow-hidden bg-wm-oxygen rounded-md overflow-hidden"
 			>
 				<img
 					src={project.cover}
-					className="min-w-full min-h-full flex place-items-center object-cover hover:scale-125 transition-all duration-500"
+					className="w-full h-full flex place-items-center object-cover hover:scale-125 transition-all duration-500"
 					alt="Project Cover"
 				/>
 			</a>
-			<div className={`grow flex flex-col ${idx % 2 == 0 ? 'items-end' : 'items-start'}`}>
+			<div className={`grow flex flex-col ${idx % 2 == 0 ? 'items-end text-right' : 'items-start text-left'}`}>
 				<p className="font-mono text-wm-oxygen">Featured Project</p>
-				<h2 className="whitespace-pre-wrap">
-					<a href={project.url} aria-label={aria}>
-						{project.title}
-					</a>
-				</h2>
+
+				<div className="flex items-center gap-2 mb-2">
+					<h2 className="whitespace-nowrap">
+						<a href={project.url} aria-label={aria}>
+							{project.title}
+						</a>
+					</h2>
+					<OpenProjectButton url={project.url ?? '#'} aria={aria} />
+				</div>
+
 				<p>{project.description}</p>
 			</div>
 		</article>
